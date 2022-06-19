a022d2b5919212467f764d6d66abb4e7ae533a06
Jangilkyu <jang19.dev@gmail.com>
2022-06-19 14:15:51 +0900

    project Init

---
+++ WalkieDoggie/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WalkieDoggie
+//
+//  Created by 장일규 on 2022/06/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
