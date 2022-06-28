1d162a519a95c9986d2ab1c075df9b9f48a13272
장일규 <jang19.dev@gmail.com>
2022-06-28 21:56:04 +0900

    Add: Email or Pwd TextFiled Type enum

---
+++ WalkieDoggie/Share/AuthTextFieldTypes.swift
@@ -0,0 +1,13 @@
+//
+//  AuthTextFieldTypes.swift
+//  WalkieDoggie
+//
+//  Created by jangilkyu on 2022/06/28.
+//
+
+import Foundation
+
+enum AuthTextFieldTypes {
+    case email
+    case password
+}
