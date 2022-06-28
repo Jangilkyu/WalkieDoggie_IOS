97caa1224af316139eace550a073a7fba3648ae5
장일규 <jang19.dev@gmail.com>
2022-06-28 15:33:33 +0900

    Add: 로그인 Label 제약사항

---
+++ WalkieDoggie/Auth/LoginController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginController.swift
+//  WalkieDoggie
+//
+//  Created by jangilkyu on 2022/06/28.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    let mainLabel: UILabel = {
+        let lb = UILabel()
+        lb.text = "로그인"
+        lb.font = WDFont.bold(size: 25)
+        lb.backgroundColor = .red
+        lb.textAlignment = .left
+        
+        return lb
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        addViews()
+        setConstraints()
+    }
+    
+    func setup() {
+        view.backgroundColor = .white
+    }
+    
+    func addViews() {
+        view.addSubview(mainLabel)
+    }
+    
+    // Constraints
+    func setConstraints() {
+        mainLabelConstraints()
+    }
+    
+    func mainLabelConstraints() {
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        mainLabel.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        mainLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        mainLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80).isActive = true
+        mainLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+    }
+}
