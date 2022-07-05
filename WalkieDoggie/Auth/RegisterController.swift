37ea9bd15c70afd1def730bc48872c9acb68b92b
장일규 <jang19.dev@gmail.com>
2022-07-05 15:59:51 +0900

    Add: email, password TextField

--- WalkieDoggie/Auth/RegisterController.swift
+++ WalkieDoggie/Auth/RegisterController.swift
@@ -17,6 +17,19 @@ class RegisterController: UIViewController {
         return lb
     }()
     
+    let emailTextField = AuthTextField(
+        titleText: "이메일",
+        placeholderText: "이메일을 입력해주세요.",
+        descriptionText: "이메일을 입력해주시기 바랍니다.",
+        type: .email)
+    
+    let pwdTextField = AuthTextField(
+        titleText: "비밀번호",
+        placeholderText: "비밀번호를 입력해주세요.",
+        descriptionText: "비밀번호는 6자리 이상이어야 합니다.",
+        type: .password
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
@@ -30,10 +43,14 @@ class RegisterController: UIViewController {
     
     func addViews() {
         view.addSubview(mainLabel)
+        view.addSubview(emailTextField)
+        view.addSubview(pwdTextField)
     }
     
     func setConstraints() {
         mainLabelConstraints()
+        emailTextFieldConstraints()
+        pwdTextFieldConstraints()
     }
     
     func mainLabelConstraints() {
@@ -42,7 +59,22 @@ class RegisterController: UIViewController {
         mainLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
         mainLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80).isActive = true
         mainLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
-
+    }
+    
+    func emailTextFieldConstraints() {
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        emailTextField.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        emailTextField.heightAnchor.constraint(equalToConstant: 88).isActive = true
+        emailTextField.topAnchor.constraint(equalTo: mainLabel.bottomAnchor, constant: 50).isActive = true
+        emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
+    }
+    
+    func pwdTextFieldConstraints() {
+        pwdTextField.translatesAutoresizingMaskIntoConstraints = false
+        pwdTextField.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        pwdTextField.heightAnchor.constraint(equalToConstant: 88).isActive = true
+        pwdTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor).isActive = true
+        pwdTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50).isActive = true
     }
     
 }
