524a83911de9ceb988a28553f0a42e4ad1482d6f
장일규 <jang19.dev@gmail.com>
2022-08-06 20:22:59 +0900

    Add: NavigationController 추가

---
+++ WalkieDoggie/MainNavigationController.swift
@@ -0,0 +1,14 @@
+//
+//  MainNavigationController.swift
+//  WalkieDoggie
+//
+//  Created by jangilkyu on 2022/08/06.
+//
+
+import UIKit
+
+class MainNavigationController: UINavigationController {
+    override func viewDidLoad() {
+      super.viewDidLoad()
+    }
+}
