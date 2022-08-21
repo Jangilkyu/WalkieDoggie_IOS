db6e45cf952793ee8c3cecc2863306047967dd0e
장일규 <jang19.dev@gmail.com>
2022-08-21 16:00:38 +0900

    Add: ProfileController 생성

---
+++ WalkieDoggie/Controllers/Profile/ProfileController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileController.swift
+//  WalkieDoggie
+//
+//  Created by jangilkyu on 2022/08/21.
+//
+
+import UIKit
+
+class ProfileController: UIViewController {
+    override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .white
+      navigationItem.setHidesBackButton(true, animated: false)
+    }
+}
