1f219cd081bb6289d14de78a61adc1b409560e7b
장일규 <jang19.dev@gmail.com>
2022-08-28 03:33:35 +0900

    Add: titleLabel 선언 후 VIew 제약 조건

--- WalkieDoggie/Controllers/Main/MainController.swift
+++ WalkieDoggie/Controllers/Main/MainController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class MainController: UIViewController {
     
+    let titleLabel: UILabel = {
+       let lb = UILabel()
+        lb.text = "가까운 유기견 센터 목록"
+        lb.font = WDFont.Medium.of(size: 23)
+       return lb
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.setHidesBackButton(true, animated: false)
@@ -23,10 +30,18 @@ class MainController: UIViewController {
     }
     
     private func addViews() {
-        
+        view.addSubview(titleLabel)
     }
     
     private func setConstraints() {
-        
+        titleLabelConstraints()
+    }
+    
+    private func titleLabelConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
     }
 }
