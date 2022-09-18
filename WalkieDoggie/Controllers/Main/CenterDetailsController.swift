dae2f9cba9a3b8b8f60dcb5c4784096335c4a11a
장일규 <jang19.dev@gmail.com>
2022-09-19 08:57:29 +0900

    Add: CollectionView에서 Detail 화면으로 이동 위한 파일 추가

---
+++ WalkieDoggie/Controllers/Main/CenterDetailsController.swift
@@ -0,0 +1,15 @@
+//
+//  CenterDetailsController.swift
+//  WalkieDoggie
+//
+//  Created by jangilkyu on 2022/09/19.
+//
+
+import UIKit
+
+class CenterDetailsController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+}
