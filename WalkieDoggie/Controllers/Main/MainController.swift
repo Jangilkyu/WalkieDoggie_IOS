d9032ac33734e66f913a51d6aae67d8645e37807
장일규 <jang19.dev@gmail.com>
2022-09-19 08:53:05 +0900

    Add: didSelectItemAt 추가

--- WalkieDoggie/Controllers/Main/MainController.swift
+++ WalkieDoggie/Controllers/Main/MainController.swift
@@ -75,7 +75,11 @@ class MainController: UIViewController {
 
 extension MainController:
     UICollectionViewDelegate {
-    
+  func collectionView(
+      _ collectionView: UICollectionView,
+      didSelectItemAt indexPath: IndexPath
+  ) {
+  }
 }
 
 extension MainController:
