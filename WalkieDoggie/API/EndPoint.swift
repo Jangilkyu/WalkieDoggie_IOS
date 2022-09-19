fd15508ea85187c2b27e3e6fdb4aacb3bb302543
장일규 <jang19.dev@gmail.com>
2022-09-19 09:13:36 +0900

    Add:  Center에 대한 엔드포인트를 정의

--- WalkieDoggie/API/EndPoint.swift
+++ WalkieDoggie/API/EndPoint.swift
@@ -9,24 +9,19 @@ import Foundation
 
 let baseURL: String = "https://wkdg.herokuapp.com/api/v1"
 
-enum EndPoint: CustomStringConvertible {
-    case register
-    case login
-    
-    var description: String {
-        switch self {
-        case .register:
-            return baseURL + "/auth/register"
-        case .login:
-            return baseURL + "/auth/login"
-        }
+enum EndPoint {
+  case register
+  case login
+  case centers
+  
+  var url: URL {
+    switch self {
+    case . register:
+      return URL(string: "\(baseURL)/auth/register")!
+    case .login:
+      return URL(string: "\(baseURL)/auth/login")!
+    case .centers:
+    return URL(string: "\(baseURL)/centers")!
     }
-    var url: URL {
-        switch self {
-            case . register:
-            return URL(string: "\(baseURL)/auth/register")!
-            case .login:
-            return URL(string: "\(baseURL)/auth/login")!
-        }
-    }
+  }
 }
