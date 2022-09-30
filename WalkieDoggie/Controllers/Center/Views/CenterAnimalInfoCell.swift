50ad7829a8cda7c75ac2fe5e1033b71268098040
장일규 <jang19.dev@gmail.com>
2022-09-30 10:58:54 +0900

    add: SubView 및 Constraint 구현을 위한 함수 선언

--- WalkieDoggie/Controllers/Center/Views/CenterAnimalInfoCell.swift
+++ WalkieDoggie/Controllers/Center/Views/CenterAnimalInfoCell.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class CenterAnimalInfoCell: UICollectionViewCell {
+  
   override init(frame: CGRect) {
-      super.init(frame: frame)
-    backgroundColor = .red
+    super.init(frame: frame)
+    backgroundColor = .systemOrange
+    setup()
   }
   
   required init?(coder: NSCoder) {
     return nil
+  }
+  
+  private func setup() {
+    addViews()
+    setConstraints()
+  }
+  
+  private func addViews() {
+  }
+  
+  private func setConstraints() {
   }
 }
