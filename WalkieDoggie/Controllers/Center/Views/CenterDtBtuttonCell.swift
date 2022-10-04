90c0f4ab96230cea2c0f093a2df7a6cf1f255263
장일규 <jang19.dev@gmail.com>
2022-10-04 09:16:23 +0900

    Add: '봉사활동 신청하기' Button Cell 구현

--- WalkieDoggie/Controllers/Center/Views/CenterDtBtuttonCell.swift
+++ WalkieDoggie/Controllers/Center/Views/CenterDtBtuttonCell.swift
@@ -9,12 +9,44 @@ import UIKit
 
 
 class CenterDtBtuttonCell: UICollectionViewCell {
+  
+  let centerButton: UIButton = {
+    let btn = UIButton(type: .custom)
+    btn.setTitle("봉사활동 신청하기", for: .normal)
+    btn.titleLabel?.font = WDFont.bold(size: 20)
+    btn.backgroundColor = WDColor.darkGray.color
+    btn.layer.cornerRadius = 10
+    return btn
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     backgroundColor = .systemOrange
+    setup()
   }
 
   required init?(coder: NSCoder) {
     return nil
+  }
+  
+  private func setup() {
+    addViews()
+    setConstraints()
+  }
+  
+  private func addViews() {
+    addSubview(centerButton)
+  }
+  
+  private func setConstraints() {
+    centerButtonConstraints()
+  }
+  
+  private func centerButtonConstraints() {
+    centerButton.translatesAutoresizingMaskIntoConstraints = false
+    centerButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    centerButton.widthAnchor.constraint(equalTo: widthAnchor).isActive = true
+    centerButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    centerButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
   }
 }
